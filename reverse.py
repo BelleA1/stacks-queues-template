22a16a1d3a88e427b2b24b5171b7b6dbe74f9a8e
BelleA1 <98564893+BelleA1@users.noreply.github.com>
2022-02-27 19:00:59 -0500

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,25 +1,31 @@
 # MODIFY ME TO IMPLEMENT YOUR SOLUTION
 # TO PROBLEM 1: REVERSE QUEUE
 #
-# NAME:         FIXME
+# NAME:         Elizabeth Ager
 # ASSIGNMENT:   Project 2: Stacks & Queues
 
+import reverse_test
 from Queue import Queue
 from Stack import Stack
 
-
 # Return a new queue in reverse order
 # Hint: can use a stack to help
-def reverse(q_orig):
-    q_new = Queue([])
+
+
+def reverse(og):
+    q_new = Queue()
+    stack_new = Stack()
+    print("og", og)
+    while (not og.is_empty()):
+        stack_new.push(og.deq())
+    while (not stack_new.is_empty()):
+        q_new.enq(stack_new.pop())
     return q_new
 
 def main():
-    q = Queue(list(range(1, 5)))
-    q.print()
-    print("reverse: ", end="")
-    reverse(q).print()
+    reverse(Queue([l for l in "Drew University"])).print()
+
 
 # Don't run main on import
-if __name__ == "__main__":
+if name == "main":
     main()
