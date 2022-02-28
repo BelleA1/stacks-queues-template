5ca3f2acf79eb9700496bac47d95693428bf2756
BelleA1 <98564893+BelleA1@users.noreply.github.com>
2022-02-28 01:59:26 -0500

    Update matching.py

--- matching.py
+++ matching.py
@@ -1,21 +1,18 @@
 # MODIFY ME TO IMPLEMENT YOUR SOLUTION
 # TO PROBLEM 2: BRACE MATCHER
 #
-# NAME:         FIXME
+# NAME:         Elizabeth Ager
 # ASSIGNMENT:   Project 2: Stacks & Queues
-
-from Stack import Stack
-
-# Returns True if the braces match,
-# & False otherwise
-def matcher(str):
-    s = Stack([])
-    return True
+def matcher(expr):
+  stack = []
+  for c in expr:
+    if c in '[({':
+      stack.append(c)
+    elif c in '])}':
+      if len(stack) == 0:
+        return False
+      if '])}'.index(c) != '[({'.index(stack.pop()):
+        return False
+  return len(stack) == 0
 
 def main():
-    print("matcher: ", matcher("[()]"))
-
-# Don't run main on import
-if __name__ == "__main__":
-    main()
-
